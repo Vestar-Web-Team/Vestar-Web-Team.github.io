1d7f2b5ca36b12de5fa1b977859a56dacc0c0998
爱走位的KN_sword <3478099037@qq.com>
2023-07-27 00:26:05 +0800

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,12 @@
 export default function Custom404() {
 	return(
-	<div className={'text-center content-center'}>
-		<h1 className={'text-4xl'}>诶呀！页面不存在！</h1>
-		<h1 className={'text-xl'}>ERROR 404</h1>
-		<button className={'text-xl border-2 border-white rounded-md m-8 p-2'}>回到主页</button>
+	<div className={''}><div className={'aboslute m-0'}>
+		</div>
+		<div className={'text-center content-center absolute my-[20%] h-[20%]'}>
+			<h1 className={'text-4xl'}>诶呀！页面不存在！</h1>
+			<h1 className={'text-xl'}>ERROR 404</h1>
+			<button className={'text-xl border-2 border-white rounded-md m-8 p-2'}>回到主页</button>
+		</div>
 	</div>
 	) 
 }
